8a1dbceee6d1279e2016b905b6dab8a9d7d22e9b
Arthur Weinberger <arthurweinberger@gmail.com>
2022-02-04 19:07:58 -0800

    foo

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 print(__name__)
 
 app = Flask(__name__)
 
 @app.route('/')
+def hello_template():
+  name = request.args.get("name", "World")
+  return render_template('index.html')
+
+@app.route('/v1')
 def hello_world():
   name = request.args.get("name", "World")
   s = "<center>Hello %s!</center>" % escape(name)
